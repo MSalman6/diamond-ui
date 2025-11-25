01b0954c163828fd0cbbc36a721bc6ebec947a97
MSalman6 <salman666online@gmail.com>
2025-11-25 20:16:45 +0500

    updated footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,36 +6,28 @@ export default function Footer() {
       <div className="container">
         <div className="footer-grid">
           <div className="footer-col">
-            <h3>DMD Diamond</h3>
+            <h3>Communities</h3>
             <ul>
-              <li><a href="https://bit.diamonds/" target="_blank">About</a></li>
-              <li><Link href="/wiki">Ecosystem</Link></li>
-              <li><a href="https://discord.com/channels/1267133854154756178/1267151457346392065" target="_blank">Community</a></li>
+              <li><a target="_blank" href="https://discord.com/invite/MwqZ2CYcB4">Discord</a></li>
+              <li><a target="_blank" href="https://t.me/DMDcoin">Telegram</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h3>Social Media</h3>
+            <ul>
+              <li><a target="_blank" href="https://www.facebook.com/dmdcoin/">Facebook</a></li>
+              <li><a target="_blank" href="https://t.me/DMDcoin">Telegram</a></li>
+              <li><a target="_blank" href="https://twitter.com/dmdcoin">Twitter</a></li>
+              <li><a target="_blank" href="https://www.linkedin.com/company/dmd-diamond/">LinkedIn</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h3>More about DMD Diamond</h3>
+            <ul>
+              <li><a href="https://bit.diamonds/" target="_blank">Website</a></li>
               <li><a href="https://bit.diamonds/blog/" target="_blank">Blog</a></li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h3>Developers</h3>
-            <ul>
-              <li><a href="https://github.com/DMDcoin/diamond-contracts-core" target="_blank">Documentation</a></li>
-              <li><a href="https://github.com/DMDcoin" target="_blank">GitHub</a></li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h3>Resources</h3>
-            <ul>
-              <li><a href="https://github.com/DMDcoin/whitepaper/wiki" target="_blank">Whitepaper</a></li>
-              <li><a href="https://github.com/DMDcoin/whitepaper/wiki/J.-Roadmap" target="_blank">Roadmap</a></li>
-              <li><Link href="/faqs">FAQs</Link></li>
-              <li><a href="https://discord.com/channels/1267133854154756178/1267406282835624029" target="_blank">Support</a></li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h3>Connect</h3>
-            <ul>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Discord</a></li>
-              <li><a href="#">Telegram</a></li>
+              <li><a href="https://dmd-diamond.medium.com/" target="_blank">Medium</a></li>
+              <li><a href="https://bitcointalk.org/index.php?topic=580725.msg64407626#msg64407626" target="_blank">Bitcointalk</a></li>
             </ul>
           </div>
         </div>
